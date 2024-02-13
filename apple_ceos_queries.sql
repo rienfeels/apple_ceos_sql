c5d3c4cbf3d8fc37bfe30ceee2a9a1457f1ce30f
RyanFields <rienfeels14@gmail.com>
2024-02-13 15:10:41 -0500

    apple CEOs sql classwork/homework completed

---
+++ apple_ceos_queries.sql
@@ -0,0 +1,12 @@
+       name       | first_year_active 
+------------------+-------------------
+ Steve Jobs       |              1976
+ Mike Markkula    |              1977
+ Mike Scott       |              1978
+ John Sculley     |              1983
+ Michael Spindler |              1993
+ Gil Amelio       |              1996
+ Steve Jobs       |              1997
+ Tim Cook         |              2011
+(8 rows)
+
